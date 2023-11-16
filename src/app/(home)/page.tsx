79d187e5031ee0252b6ca66c5fe9e170442b5d62
Paulo Ricardo Malta Leal <paulo.leal@a4pm.com.br>
2023-11-16 16:00:53 -0300

    feat: form and label component, start homepage.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Image from 'next/image'
+import React from 'react'
 
 function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className='mx-3 my-20 h-full w-auto rounded-lg bg-slate-200 px-2'>
+      <h1 className='text-lg font-bold uppercase'>IA Calculator</h1>
+      <div className='items-center justify-center p-5'>
+        <Image
+          height={0}
+          width={300}
+          alt='personagem indeciso sobre o que comer'
+          src='/first-image.png'
+        ></Image>
+      </div>
+      <div>
+        <h1 className='font-medium'>Se alimente de forma inteligente!</h1>
+      </div>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
