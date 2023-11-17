1330b5bb24f07f111e3743c9700675117a2ecc44
Paulo Ricardo Malta Leal <paulo.leal@a4pm.com.br>
2023-11-17 12:53:40 -0300

    feat: add Home page and welcome component

---
+++ src/app/(home)/components/welcome.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+export const Welcome = () => {
+  return (
+    <>
+      <div className=' mb-10 flex justify-center pt-8 text-3xl'>
+        <h1 className='font-kdam font-light'>AI</h1>
+        <h1 className='font-semibold'>Diets</h1>
+      </div>
+
+      <div className='mb-24 flex h-80 w-full justify-center'>
+        <div className='absolute top-20'>
+          <Image width={400} height={400} alt='Background Image' src='/first-image.png'></Image>
+        </div>
+        <Image
+          width={0}
+          height={0}
+          alt='Background Image'
+          src='/background_image.svg'
+          className='flex h-[450px] w-[100vw]'
+        ></Image>
+      </div>
+
+      <div className='text-center'>
+        <span className='text-4xl font-bold  '>Bem vindo à Calculdora de Dietas Inteligente!</span>
+        <p className='pt-4 text-lg'>
+          Siga os passos para ter seu plano alimentar de acordo com seu objetivo.
+        </p>
+      </div>
+      <div className='mb-7 mt-7 flex items-center justify-center'>
+        <Button className='h-14 w-36 text-lg'>Começar</Button>
+      </div>
+    </>
+  )
+}
