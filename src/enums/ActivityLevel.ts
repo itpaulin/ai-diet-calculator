1f28ca871d2e640d496431b5e81f2c26c0387b62
Paulo Ricardo <prmaltaleal@gmail.com>
2023-12-29 00:38:12 -0300

    feat: improve text to field activity levels

--- src/enums/ActivityLevel.ts
+++ src/enums/ActivityLevel.ts
@@ -1,7 +1,7 @@
 enum ActivityLevel {
-  Sedentary = 'Sedentário',
-  ModeratelyActive = 'Moderamente Ativo',
-  VeryActive = 'Bastante Ativo',
+  Sedentary = 'Sedentário - passa o dia sentado / trabalho em escritório',
+  ModeratelyActive = 'Moderamente Ativo - passa parte do dia caminhando ou fazendo alguma atividade',
+  VeryActive = 'Bastante ativo - faz trabalho braçal ou atividades físicas intensas',
 }
 
 export default ActivityLevel
