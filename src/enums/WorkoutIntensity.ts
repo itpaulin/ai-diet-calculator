b5d4e87cee7c308aed300919841cc41368403eae
Paulo Ricardo <prmaltaleal@gmail.com>
2023-12-29 00:30:16 -0300

    feat: enum of workout intensity

---
+++ src/enums/WorkoutIntensity.ts
@@ -0,0 +1,6 @@
+enum WorkoutIntensity {
+  Light = 'Leve (usa cargas baixas e a velocidade de batimentos cardíacos não fica elevada)',
+  Moderate = 'Moderado (treina usando cargas desafiadoras -75-80% RM - e batimentos ficam acelerados)',
+  Intense = 'Alta Intensidade (treino com o máximo de cargas, pouco descanso e/ou técnicas avançadas)',
+}
+export default WorkoutIntensity
