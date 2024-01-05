32f02bcbe9f2e339bdda612b6efc969c5ec320de
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-05 11:47:23 -0300

    feat: add custom macro nutrients form

--- src/app/metricas/components/macros.tsx
+++ src/app/metricas/components/macros.tsx
@@ -1,4 +1,13 @@
-import { Form, FormControl, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
@@ -7,9 +16,9 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 const FormSchemaCustom = z.object({
-  fat: z.coerce.number().positive(),
-  protein: z.coerce.number().positive(),
-  carbohydrate: z.coerce.number().positive(),
+  fat: z.coerce.number().positive().max(100),
+  protein: z.coerce.number().positive().max(100),
+  carbohydrate: z.coerce.number().positive().max(100),
 })
 const Macros = () => {
   const form = useForm({
@@ -24,26 +33,63 @@ const Macros = () => {
     console.log(values)
   }
   return (
-    <div className='flex flex-row justify-center '>
-      <Tabs className='pt-4' defaultValue='preset'>
+    <div>
+      <Tabs className=' pt-4 text-center' defaultValue='preset'>
         <TabsList className='rounded-sm bg-primary text-black'>
           <TabsTrigger value='preset'>Predefinida G</TabsTrigger>
           <TabsTrigger value='custom'>Percentual %</TabsTrigger>
         </TabsList>
-        <TabsContent value='preset'>
+        <TabsContent value='custom'>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              {/* <FormField 
-                control={form.control}
-                name=""
-                render={({field}) = > (
-                  <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem> */}
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className='flex flex-col items-center justify-center text-center'
+            >
+              <Label className='p-5 text-lg font-semibold'>
+                Informe a porcentagem % que deseja de acordo com os macro nutrientes:
+              </Label>
+              <div className='flex flex-col gap-8'>
+                <FormField
+                  control={form.control}
+                  name='protein'
+                  render={({ field }) => (
+                    <FormItem className='grid grid-cols-2'>
+                      <FormLabel className='pt-3'>Proteínas</FormLabel>
+                      <FormControl>
+                        <Input type='number' placeholder='%' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name='fat'
+                  render={({ field }) => (
+                    <FormItem className='grid grid-cols-2'>
+                      <FormLabel className='pt-3'>Gorduras</FormLabel>
+                      <FormControl>
+                        <Input type='number' placeholder='%' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name='carbohydrate'
+                  render={({ field }) => (
+                    <FormItem className='grid grid-cols-2'>
+                      <FormLabel className='pt-3'>Carboidratos</FormLabel>
+                      <FormControl>
+                        <Input type='number' placeholder='%' {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type='submit'>Submit</Button>
+              </div>
             </form>
           </Form>
         </TabsContent>
