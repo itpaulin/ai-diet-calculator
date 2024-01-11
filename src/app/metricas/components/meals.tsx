070fbf47e27d296874df52a1ced36dbc725df775
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-10 23:50:12 -0300

    feat: add table to show calories for meal.

--- src/app/metricas/components/meals.tsx
+++ src/app/metricas/components/meals.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 import { useState } from 'react'
 
 const Meals = () => {
-  const [mealPerDay, setMealPerDay] = useState<number[]>([3, 4, 5, 6])
+  const [mealPerDay, setMealPerDay] = useState<number[]>([2, 3, 4, 5, 6])
   const [quantity, setQuantity] = useState<number>(3)
   const handleQuantity = (value: number) => {
     setQuantity(value)
@@ -13,9 +13,11 @@ const Meals = () => {
         <span className='text-xl font-semibold'>Seus Macros Nutrientes Diários:</span>
         <span>Carboidratos: 100g | Proteínas: 250g | Gordura: 75g </span>
       </div>
-      <span>Escolha como dividí-los durante o seu dia</span>
-      <div className='flex flex-row'>
-        <span className='pr-2 pt-2'>Quantidade de refeições:</span>
+      <span className='flex justify-center text-xl font-semibold'>
+        Escolha como dividí-los durante o seu dia
+      </span>
+      <div className='flex flex-col pt-3'>
+        <span className='pr-2 pt-2 text-center font-normal '>Quantidade de refeições:</span>
         <div className='grid-cols-6 items-center justify-center gap-2 text-center'>
           {mealPerDay.map((value) => (
             <>
@@ -28,6 +30,32 @@ const Meals = () => {
             </>
           ))}
         </div>
+      </div>
+      <div className='flex items-center justify-center pt-6'>
+        <table className='border'>
+          <thead className='border'>
+            <tr>
+              <th className='p-5 text-xl'>100 Calorias por refeição</th>
+            </tr>
+          </thead>
+          <tbody className='flex'>
+            <tr className='flex flex-col '>
+              <td className='border pr-16'>Carboidratos</td>
+              <td className='border'>Proteínas</td>
+              <td className='border'>Gorduras</td>
+            </tr>
+            <tr className='flex flex-col '>
+              <td className='border pr-16'>10g</td>
+              <td className='border'>25g</td>
+              <td className='border'>7.5g</td>
+            </tr>
+            <tr className='flex flex-col'>
+              <td className='border pr-16'>50%</td>
+              <td className='border'>10%</td>
+              <td className='border'>40%</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   )
