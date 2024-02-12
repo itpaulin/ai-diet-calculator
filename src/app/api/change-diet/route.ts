00760233c501fb144fceb04f573baf79235dc686
Paulo Ricardo <prmaltaleal@gmail.com>
2024-02-12 00:59:04 -0300

    refactor: remove unnecessary comments

--- src/app/api/change-diet/route.ts
+++ src/app/api/change-diet/route.ts
@@ -8,14 +8,11 @@ const openai = new OpenAI({
 })
 
 export async function POST(req: Request) {
-  // Extract the `prompt` from the body of the request
   const { prompt } = await req.json()
 
-  // Request the OpenAI API for the response based on the prompt
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     stream: true,
-    // a precise prompt is important for the AI to reply with the correct tokens
     messages: [
       {
         role: 'user',
